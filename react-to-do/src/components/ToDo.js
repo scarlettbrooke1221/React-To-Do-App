93fc17725fd8377034b0eb0db779d1cae8c676d0
erica mitchell <ericamitchell1221@gmail.com>
2018-06-07 19:55:50 -0400

    Checkpoint 8 Props And STate: COMPLETED

--- react-to-do/src/components/ToDo.js
+++ react-to-do/src/components/ToDo.js
@@ -4,7 +4,10 @@ import React, { Component } from 'react';
  class ToDo extends Component {
    render() {
      return (
-       <li> {this.props.description} </li>
+       <li> 
+	   <input type="checkbox" checked={ this.props.isCompleted} />
+	   <span>{ this.props.description } </span>
+	   </li>
      );
    }
  }
