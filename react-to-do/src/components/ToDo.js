b906ce46de31f5567f98beac6b0ce50f8200ee09
erica mitchell <ericamitchell1221@gmail.com>
2018-06-14 19:16:30 -0400

    Checkpoint 9 EVENTS: Completed

--- react-to-do/src/components/ToDo.js
+++ react-to-do/src/components/ToDo.js
@@ -7,7 +7,9 @@ import React, { Component } from 'react';
        <li> 
 	     <input type="checkbox" checked={ this.props.isCompleted } onChange={ this.props.toggleComplete } />
          <span>{this.props.description}</span>
+		 <button onClick={this.props.deleteTodo}>Delete</button>
        </li>
+	   
      );
    }
  }
